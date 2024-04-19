02df9c1c8705979e0ea898b7a0f3bc0a5e02ba15
Ahmad Huda Salam <ahmadhudasalam9@gmail.com>
2024-04-19 08:03:35 +0700

    update config powershell

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-oh-my-posh init pwsh --config "C:\Users\huda\Documents\PowerShell\robbyrussell_gruvbox.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\huda\Documents\PowerShell\robbyrussell_solarized.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
